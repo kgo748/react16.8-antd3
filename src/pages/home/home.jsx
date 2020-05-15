a0db6f50de75b0eef900097584b0ce8aea827b99
kgo748 <2085894077@qq.com>
2020-05-15 17:44:59 +0800

    首次提交，完成category，product，role部分，antd.v.3.xxx

---
+++ src/pages/home/home.jsx
@@ -0,0 +1,17 @@
+/**
+ * 首页路由
+ */
+import React,{ Component } from "react";
+
+import "./home.less";
+
+
+export default class Category extends Component {
+  render() {
+    return (
+        <div className="home">
+          欢迎使用不知名的后台管理系统
+        </div>
+    );
+  }
+}
---
+++ src/pages/user/user.jsx
@@ -0,0 +1,15 @@
+/**
+ * 用户路由
+ */
+import React,{ Component } from "react";
+
+export default class User extends Component {
+  render() {
+
+    return (
+        <div>
+          User
+        </div>
+    );
+  }
+}
